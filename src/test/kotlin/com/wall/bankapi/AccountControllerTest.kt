25d2be1717a913d37a3262146552f1fcabd453d7
Wallisson Martins <wallissonmartins37@gmail.com>
2023-10-07 11:13:19 -0300

    modify unit test

--- src/test/kotlin/com/wall/bankapi/AccountControllerTest.kt
+++ src/test/kotlin/com/wall/bankapi/AccountControllerTest.kt
@@ -92,7 +92,7 @@ class AccountControllerTest {
     @Test
     fun `test update account`() {
         val account = accountRepository
-            .save(Account(name = "João Teste", document = "123", phone = "77774444"))
+            .save(Account(name = "João Teste", document = "123666", phone = "77774444"))
             .copy(name = "Updated")
         val json = ObjectMapper().writeValueAsString(account)
 
